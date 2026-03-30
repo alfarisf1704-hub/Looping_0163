7398e198678360ecbc44d989dbd1ba34cc4a45bb
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 16:20:59 +0700

    menambahkan keterangan

--- looping.cpp
+++ looping.cpp
@@ -5,15 +5,17 @@ using namespace std;
 int main()
 {
     int x;
-
+    // PERULANGAN DENGAN FOR
+    // MENCETAK SEBANYAK 5 KALI
     cout << "PERULANGAN FOR" << endl;
     for (int i = 0; i <= 4; i++)
     {
         cout << "TEKNOLOGI INFORMASI UMY" << endl;
     }
     cout << endl;
+    // UNTUK MENDAPATKAN URUTAN ANGKA ACAK YANG BERBEDA
     srand(time(0));
-
+    // PERULANGAN DENGAN WHILE
     cout << "PERULANGAN WHILE" << endl;
     x = 1 + rand() % 10;
 }
