7c910bb95b44748f4771d7405c362f15f8ad655b
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 17:19:25 +0700

    perbaikan hasil seleksi

--- switchCase.cpp
+++ switchCase.cpp
@@ -45,7 +45,7 @@ void pilihanJurusan()
 // prosedur output
 void output()
 {
-    cout << "\n===HASIL SELEKSI===" << namaJurusan << endl;
+    cout << "\n===HASIL SELEKSI===" << endl;
     cout << "Jurusan Pilihan  : " << namaJurusan << endl;
     cout << "Status Kuota     : " << statusKuota << endl;
     cout << "-----------------------------------" << endl;
