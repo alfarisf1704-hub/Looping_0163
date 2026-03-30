28e5c1f09496ded033923d0be4fea180377c4a59
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 16:30:36 +0700

    membuat perulangan while dan do..while

--- looping.cpp
+++ looping.cpp
@@ -15,7 +15,28 @@ int main()
     cout << endl;
     // UNTUK MENDAPATKAN URUTAN ANGKA ACAK YANG BERBEDA
     srand(time(0));
+
     // PERULANGAN DENGAN WHILE
     cout << "PERULANGAN WHILE" << endl;
     x = 1 + rand() % 10;
+
+    while (x <= 5)
+    {
+        cout << "BILANGAN ACAK = " << x << endl;
+        x = 1 + rand() % 10;
+
+        cout << "BILANGAN ACAK WHILE YG TERAKHIR = " << x << endl;
+        cout << endl;
+
+        cout << "PERULANGAN DO....WHILE" << endl;
+        x = 1 + rand() % 10;
+        do
+        {
+            cout << "BILANGAN ACAK = " << x << endl;
+            x = rand() % 10;
+        } while (x <= 5);
+
+        cout << "BILANGAN ACAK DO-WHILE YG TERAKHIR = " << x << endl;
+        cout << endl;
+    }
 }
