04b740679abd90d60b54e9742ec4a67b6805f1db
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 17:11:01 +0700

    membuat void pilihan jurusan

--- switchCase.cpp
+++ switchCase.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // DEKLARASI VARIABLE GLOBAL
 int kodeJurusan;
 string namaJurusan;
-string statusKota;
+string statusKuota;
 
 // prosedur input
 void input()
@@ -16,4 +16,28 @@ void input()
     cout << "--------------------------------" << endl;
     cout << "---Masukan Kode Jurusan (1-3): ";
     cin >> kodeJurusan;
+}
+
+// prosedur untuk pengecekan kondisi
+void pilihanJurusan()
+{
+    switch (kodeJurusan)
+    {
+    case 1:
+        namaJurusan = "Teknologi Informasi";
+        statusKuota = "Tersedia (15 kursi)";
+        break;
+    case 1:
+        namaJurusan = "Teknik Elektro";
+        statusKuota = "Kuota Penuh!";
+        break;
+    case 1:
+        namaJurusan = "Sistem Informasi";
+        statusKuota = "Tersedia (5 kursi)";
+        break;
+    default:
+        namaJurusan = "Tidak Diketahui";
+        statusKuota = "Error: Kode tidak valid";
+        break;
+    }
 }
